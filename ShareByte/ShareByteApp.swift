d44dc2a2963b001cd446f2de58a9f1fce0334c1c
Mityunin Dmitry <mity-ml@mail.ru>
2023-07-08 12:10:29 +0500

    Initial Commit

---
+++ ShareByte/ShareByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareByteApp.swift
+//  ShareByte
+//
+//  Created by Dmitry Mityunin on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ShareByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
